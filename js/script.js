391438bfb404a4005b3252881cb48b856c149851
Jayendra <jay08jog@gmail.com>
2015-11-21 11:39:39 -0800

    functional grid

--- js/script.js
+++ js/script.js
@@ -2,9 +2,32 @@ $the_url = "https://api.indiegogo.com/1/search/campaigns.json?api_token=e377270b
 $.getJSON( $the_url, function( data ) {
 		$.each(data, function(obj, key, value){
 			var titles = [];
+			var counter = 0;
 			for (var i in key)
-				titles.push('<li>' + key[i].title + '</li>');	
-			$('#toadd').append(titles.join(''));		
+			{
+				if(counter % 4 == 0)
+					titles.push('<div class="row">');
+				titles.push('<div class="grid-item"><div class="name">' + key[i].title 
+					+ '</div><img class="pic" src ="' + key[i].image_types.small 
+					+ '"><div class="subscript">'+ key[i].tagline + '</div></div>');
+				if((counter+1)%4== 0)
+				{
+					titles.push('</div>');
+					counter = 0;
+				}
+				else counter++;
+			}
+			$('#grid').append(titles.join(''));		
 		});
 });
+$('#container').masonry({
+    // options
+    itemSelector : '.item',
+    columnWidth : 200
+  });
 
+$('.grid').isotope({
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
